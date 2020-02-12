9fa8d0ddfb5427ffb05a22ab8fdbbb9a876503ec
Daniel Waiguru <52042764+DanielWaiguru91@users.noreply.github.com>
2020-02-13 01:11:16 +0300

    Handling non existing page errors using star route

--- ExpressJS/FirstExpressJSApp/app.js
+++ ExpressJS/FirstExpressJSApp/app.js
@@ -3,18 +3,23 @@ const app = express();
 
 app.get('/', (req, res)=>{
     res.send('<a href="/contact">Contact Us</a> <a href="/services">Services</a> <a href="/about">About Us</a>');
-})
+});
 app.get('/services', (req, res)=>{
     res.send('<h1>Services We Offer Include:</h1>')
 })
 app.get('/about', (req, res)=>{
     res.send('<h1>About Us Page:</h1>');
-})
+});
 
 app.get('/contact', (req, res)=>{
     res.send('<h1>Contact Us Page</h1>');
-})
+});
+
+//handling Error routes using star route
+app.get('*', (req, res)=>{
+    res.send('Error 404. The Page Doest not Exist. <a>Go Back</a>');
+});
 
 app.listen(4000, ()=>{
     console.log('Server started on port 4000.');
-})
+});
