4d45931afbc731fbe797e8137c21f4647e561670
Daniel Waiguru <danielirungu91@gmail.com>
2020-02-23 15:34:45 +0300

    using route parameters

---
+++ ExpressJS/RouteParameters/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+/*
+An example to handle route parameters of codeproject site for Q&A
+https://www.codeproject.com/Articles/43438/Connect-C-to-MySQL
+*/
+
+app.route('/Articles/:id/:title/', (req, res)=>{
+    res.send('This route handles all the questions');
+});
+
+
+/*
+Handles code project members profiles
+*/
+app.route('/Members/:username', (req, res)=>{
+    let user = req.params.username;
+    res.send('This route handles codeproject members profile');
+});
+
+
+app.listen(4000, ()=>{
+    console.log('Server started at port 4000.');
+});
